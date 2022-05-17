3319f7655cc3bae450fd2df2cbfb65fc23272308
Anton Komrachkov <a.komrachkov@gmail.com>
2022-05-17 11:38:56 +0300

    Add try with resurses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         String host = "127.0.0.1";
-        int port = 8080;
+        int port = 8081;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
